fdcea7e43f429ca91e401085e238a33a0fff8167
Eryk Alexandre Doucet <eadoucet@gmail.com>
2020-11-11 12:37:05 -0500

    added seed file and route to model review and restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurant = Restaurant.create!([{ name: "Epicure", category: "french", address: "123 nowhere street"},
+  {name: "benny", category: "japanese", address: "4234 someplace street"},
+  {name: "mcdonald", category: "italian", address: "77 devils street", phone_number: "672-389-7654"},
+  {name: "thai express", category: "belgian", address: "9882 placeholder street"},
+  {name: "amir", category: "belgian", address: "1931 lost street"}])
